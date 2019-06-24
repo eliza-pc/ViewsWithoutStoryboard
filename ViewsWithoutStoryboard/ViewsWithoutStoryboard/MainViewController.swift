6aafafc1b17d631675f1781cd65e8c4dd6387f72
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-06-23 21:12:34 -0400

    Add button to MainVC

--- ViewsWithoutStoryboard/ViewsWithoutStoryboard/MainViewController.swift
+++ ViewsWithoutStoryboard/ViewsWithoutStoryboard/MainViewController.swift
@@ -17,15 +17,41 @@ class MainViewController: UIViewController {
         
         view.backgroundColor = .green
         
+        addButton()
+        
     }
     
     
-    func buttonTapped(){
+    @objc func buttonTapped(){
+        print("button Tapped")
+        let detailViewController = DeatilViewController()
+        navigationController?.pushViewController(detailViewController, animated: true)
         
     }
     
     private func addButton(){
+        let mainButton = UIButton()
         
+        mainButton.translatesAutoresizingMaskIntoConstraints = false
+        mainButton.addTarget(self, action: #selector(MainViewController.buttonTapped), for: .touchUpInside)
+        mainButton.setTitle("Go to Hell!", for: .normal)
+        mainButton.setTitleColor(.red, for: .normal)
+        mainButton.backgroundColor = .blue
+        
+        view.addSubview(mainButton)
+        
+        
+        let horizontalCenter = NSLayoutConstraint(item: mainButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+        
+        let verticalCenter = NSLayoutConstraint(item: mainButton, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0)
+        
+        let width = NSLayoutConstraint(item: mainButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 100)
+        
+        let heigth = NSLayoutConstraint(item: mainButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 75)
+        
+        var constraits: [NSLayoutConstraint] = [horizontalCenter,verticalCenter, width, heigth]
+        
+        NSLayoutConstraint.activate(constraits)
         
     }
 }
