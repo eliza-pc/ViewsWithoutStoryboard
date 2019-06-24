590327dbe744368aa237b5aad5304b2f4738ef10
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-06-23 20:01:29 -0400

    Add MainViewController file

---
+++ ViewsWithoutStoryboard/ViewsWithoutStoryboard/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  ViewsWithoutStoryboard
+//
+//  Created by Eliza Maria Porto de Carvalho on 23/06/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        
+    }
+}
