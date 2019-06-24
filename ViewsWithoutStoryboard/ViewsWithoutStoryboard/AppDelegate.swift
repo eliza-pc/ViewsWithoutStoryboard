49aa5fdfc101bfbbc5b545b609ac568df6eff57c
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-06-23 20:21:40 -0400

    Adding View Calling methods in AppDegate

--- ViewsWithoutStoryboard/ViewsWithoutStoryboard/AppDelegate.swift
+++ ViewsWithoutStoryboard/ViewsWithoutStoryboard/AppDelegate.swift
@@ -15,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let mainTabBarController = MainTabBarController()
+        
+        let mainNavigationController = MainNavigationController()
+        let secondaryViewController = SecondaryViewController()
+        
+        mainTabBarController.viewControllers = [mainNavigationController, secondaryViewController]
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = mainTabBarController
         // Override point for customization after application launch.
         return true
     }
