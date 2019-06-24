cd01ff1575f595a47678bb655c1785bf89268cef
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-06-23 20:07:45 -0400

    Add SecondaryViewController and DetailViewController File

---
+++ ViewsWithoutStoryboard/ViewsWithoutStoryboard/SecondaryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondaryViewController.swift
+//  ViewsWithoutStoryboard
+//
+//  Created by Eliza Maria Porto de Carvalho on 23/06/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondaryViewController:  UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
